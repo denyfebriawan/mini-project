8b8f4edef7482da29b1d971b5f2306808a5a0037
Deny Febriawan <denyfebriawan@gmail.com>
2022-10-19 20:50:20 +0700

    ambil perubahan

--- app.py
+++ app.py
@@ -25,7 +25,12 @@ def bucket_post():
     db.gudangku.insert_one(doc)
 
     return jsonify({'msg': 'data saved!'})
-    
+
+@app.route("/delete", methods=["POST"])
+def delete_bucket():
+    num_receive = request.form['num_give']
+    db.bucket.delete_one({'num': int(num_receive)})
+    return jsonify({'msg': 'delete done!'})
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
