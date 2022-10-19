b4f94f03ad948b9bb022c45894c19a46946b14f4
William Sigalemba <williamsigalemba@elektro.polimdo.ac.id>
2022-10-19 20:56:23 +0800

    delete function done

--- app.py
+++ app.py
@@ -6,9 +6,32 @@ db = client.dbsparta
 
 app = Flask(__name__)
 
+
+
 @app.route('/')
 def home():
     return render_template('index.html')
+
+@app.route("/gudang", methods=["POST"])
+def bucket_post():
+    item_name_receive = request.form['item_name_give']
+    item_quantity_receive = request.form['item_quantity_give']
+    count = db.bucket.count_documents({})
+
+    doc = {
+        'name' : item_name_receive,
+        'quantity' : item_quantity_receive,
+    }
+
+    db.gudangku.insert_one(doc)
+
+    return jsonify({'msg': 'data saved!'})
+
+@app.route("/delete", methods=["POST"])
+def delete_bucket():
+    num_receive = request.form['num_give']
+    db.bucket.delete_one({'num': int(num_receive)})
+    return jsonify({'msg': 'delete done!'})
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
---
+++ readme.md
@@ -0,0 +1,8 @@
+Setelah clone project silahkan install virtual environment dulu
+    
+    python -m venv venv
+
+Jika sudah install virtual environment silahkan install beberapa library berikut
+
+    pip install flask dnspython pymongo
+    
